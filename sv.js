38503ca8a9624b0a2034c7be344a1f29377f5c9e
osis1 <72921179+osis1@users.noreply.github.com>
2026-08-31 05:40:57 +0300

    sv.js

--- sv.js
+++ sv.js
@@ -1,1 +1,10 @@
+// Ждем 5 секунд после загрузки и шлем уведомление
+setTimeout(function() {
+    self.registration.showNotification('Тест ПУША!', {
+        body: 'Если ты видишь это при закрытом приложении — ПУШИ РАБОТАЮТ!',
+        vibrate: [500, 200, 500]
+    });
+}, 5000);
 
+self.addEventListener('install', function() { self.skipWaiting(); });
+self.addEventListener('activate', function(event) { event.waitUntil(clients.claim()); });

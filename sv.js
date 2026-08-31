177434608b3720e9b7f0402f7c517d313642c65c
osis1 <72921179+osis1@users.noreply.github.com>
2026-08-31 05:44:19 +0300

    Update sv.js

--- sv.js
+++ sv.js
@@ -1,10 +1,9 @@
-// Ждем 5 секунд после загрузки и шлем уведомление
+self.addEventListener('install', function() { self.skipWaiting(); });
+self.addEventListener('activate', function(event) { event.waitUntil(clients.claim()); });
+
 setTimeout(function() {
     self.registration.showNotification('Тест ПУША!', {
         body: 'Если ты видишь это при закрытом приложении — ПУШИ РАБОТАЮТ!',
         vibrate: [500, 200, 500]
     });
 }, 5000);
-
-self.addEventListener('install', function() { self.skipWaiting(); });
-self.addEventListener('activate', function(event) { event.waitUntil(clients.claim()); });

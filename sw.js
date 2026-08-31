495ca7ad270a67e7a2d4aa0f2eb7490119bfdeed
osis1 <72921179+osis1@users.noreply.github.com>
2026-08-31 06:18:38 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', function() { self.skipWaiting(); });
-self.addEventListener('activate', function(e) { e.waitUntil(clients.claim()); });
-self.addEventListener('message', function(e) {
-    if (e.data === 'test') {
-        setTimeout(function() {
-            self.registration.showNotification('Тест ПУША!', { body: 'Работает при свернутом приложении!', vibrate: [500, 200, 500] });
-        }, 5000);
-    }
-});

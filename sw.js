3e8d0d1fdfbda7e7edd17d371472b223ec1423e4
osis1 <72921179+osis1@users.noreply.github.com>
2026-08-31 06:19:00 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-
+self.addEventListener('install', function() { self.skipWaiting(); });
+self.addEventListener('activate', function(e) { e.waitUntil(clients.claim()); });
+self.addEventListener('message', function(e) {
+    if (e.data === 'test') {
+        setTimeout(function() {
+            self.registration.showNotification('Тест ПУША!', { body: 'Работает!', vibrate: [500, 200, 500] });
+        }, 5000);
+    }
+});

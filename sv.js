372ecd6cda0f30178fdc3c997fd64d6ba429144f
osis1 <72921179+osis1@users.noreply.github.com>
2026-08-31 06:03:33 +0300

    sv.js

--- sv.js
+++ sv.js
@@ -1,15 +1,9 @@
 self.addEventListener('install', function() { self.skipWaiting(); });
-self.addEventListener('activate', function(event) { event.waitUntil(clients.claim()); });
-
-// Слушаем команды от сайта (от кнопки теста)
-self.addEventListener('message', function(event) {
-    if (event.data && event.data.type === 'TEST_PUSH') {
-        // Ждем 5 секунд и показываем системное уведомление
+self.addEventListener('activate', function(e) { e.waitUntil(clients.claim()); });
+self.addEventListener('message', function(e) {
+    if (e.data === 'test') {
         setTimeout(function() {
-            self.registration.showNotification('🐴 Тест ПУША!', {
-                body: 'Оно работает даже если сайт свернут!',
-                vibrate: [500, 200, 500]
-            });
+            self.registration.showNotification('Тест ПУША!', { body: 'Работает при свернутом приложении!', vibrate: [500, 200, 500] });
         }, 5000);
     }
 });
